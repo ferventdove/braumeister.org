651946d0d8f959d3d44e60d072b8151d01b99a29
Sebastian Staudt <koraktor@gmail.com>
2012-01-08 12:17:11 +0100

    Use a nicer URL for searching

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,19 +2,27 @@
 //= require jquery_ujs
 
 var formHandler = function() {
-  if($('input#search').val() == $('input#search')[0].defaultValue) {
-    $('input#search').addClass('inactive');
+  if($('#search').val() == $('#search')[0].defaultValue) {
+    $('#search').addClass('inactive');
   }
 
-  $('input#search').live('focus', function() {
+  $('#search').live('focus', function() {
     $(this).removeClass('inactive');
     this.value = '';
   });
 
-  $('input#search').blur(function() {
+  $('#search').blur(function() {
     if($(this).val().match(/^\s*$/)) {
       $(this).addClass('inactive');
       this.value = this.defaultValue;
     }
+  });
+
+  $('#search-form').submit(function() {
+      if(!$('#search').hasClass('inactive')) {
+          window.location = '/search/' + $('#search').val();
+      }
+
+      return false;
   });
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Braumeister::Application.routes.draw do
 
+  match '/search/:search' => 'formula#index'
+
   root :to => 'formula#index'
 
 end
