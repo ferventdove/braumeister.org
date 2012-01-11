eb7b425dad5866d4bac69cc14ccabb22b7803373
Sebastian Staudt <koraktor@gmail.com>
2012-01-11 12:23:10 +0100

    Improved compatibility with older Git versions
    Heroku's Cedar dynos have only Git 1.7.0.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -83,7 +83,9 @@ class Repository
   def generate_history(last_sha = nil)
     ref = last_sha.nil? ? 'HEAD' : "#{last_sha}..HEAD"
 
-    log = git "log --format=format:'%H%x00%ct%x00%aE%x00%aN%x00%s' --name-status --no-merges --reverse #{ref} -- 'Formula/*.rb' 'Library/Formula/*.rb'"
+    Rails.logger.info "Regenerating history for #{ref}"
+
+    log = git "log --format=format:'%H%x00%ct%x00%aE%x00%aN%x00%s' --name-status --no-merges --reverse #{ref} -- 'Formula' 'Library/Formula'"
     revisions = []
     commits = log.split(/\n\n/)
     commits.each do |commit|
