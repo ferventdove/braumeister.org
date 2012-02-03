cb576a8c82528d0354f51da77beae54c40c16b21
Sebastian Staudt <koraktor@gmail.com>
2012-02-03 15:37:51 +0100

    Use the RSpec documentation formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,15 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+# This code is free software; you can redistribute it and/or modify it under
+# the terms of the new BSD License.
+#
+# Copyright (c) 2012, Sebastian Staudt
+
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
 RSpec.configure do |config|
+  config.formatter = :documentation
   config.mock_with :mocha
 
   config.infer_base_class_for_anonymous_controllers = true
