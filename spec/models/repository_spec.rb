504c329a8093d8de0b7987dab7a2051a6bd481e3
Sebastian Staudt <koraktor@gmail.com>
2012-05-10 20:25:12 +0200

    Fixed repository specs

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -170,6 +170,7 @@ describe Repository do
 
       Object.expects(:remove_const).with :Formula
       repo.expects(:load).with 'global.rb'
+      repo.expects(:load).with 'formula.rb'
     end
 
     it 'clones or updates the main repository for non-full repositories' do
